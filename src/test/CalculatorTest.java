32ec2b60b0a69c02ae5184c4e060dec8dd80d7c1
Marek <marwm@op.pl>
2016-05-14 10:37:45 +0200

    dodane testy klasy Calculator

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -3,6 +3,9 @@ package test;
 import main.Calculator;
 
 import org.junit.Before;
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -24,6 +27,46 @@ public class CalculatorTest {
 	@Test
 	public void test1() throws Exception {
 		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 5.5;
+		
+		double wynik = classUnderTest.sum(a, b);
+
+		assertEquals(9.5, wynik, 0.001);		
 	}
 
+	@Test
+	public void testSumowanieTrzech() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 1.0;
+		double b = 2.0;
+		double c = 1.33;
+		
+		double wynik = classUnderTest.sum(a, classUnderTest.sum(b, c));
+
+		assertEquals(4.33, wynik, 0.001);		
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 2.0;
+		double b = 3.1;
+		
+		double wynik = classUnderTest.multiply(a, b);
+
+		assertEquals(6.2, wynik, 0.001);		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void test3() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 3.0;
+		double b = 0;
+		
+		double wynik = classUnderTest.divide(a, b);
+
+		assertEquals(1.5, wynik, 0.001);
+		
+	}
+	
 }
